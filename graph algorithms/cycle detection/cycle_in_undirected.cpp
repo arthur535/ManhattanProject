eda25483e6d97d44bf4e87c018823b3e1c082aef
Heisenberg <109768662+arthur535@users.noreply.github.com>
2024-01-26 01:03:43 +0400

    Update cycle_in_undirected.cpp

--- graph algorithms/cycle detection/cycle_in_undirected.cpp
+++ graph algorithms/cycle detection/cycle_in_undirected.cpp
@@ -1,1 +1,27 @@
-
+//function finds if there is a cycle in undirected graph or not
+bool has_cycle_undirected(const vector<Node*>& vertices) {
+    unordered_map<Node*, State> visited;
+    stack<Node*> s;
+    for(int i = 0; i < vertices.size(); ++i){
+        Node* u = vertices[i];
+        if(!visited[u]){
+            s.push(u);
+            visited[u] = INSTACK;
+            while(!s.empty()){
+                Node* top = s.top();
+                s.pop();
+                visited[top] = VISITED;
+                for(Node* adj : top->adjacents){
+                    if(visited[adj] == INSTACK) {
+                       return true;
+                    }
+                    if(visited[adj] == 0){
+                        s.push(adj);
+                        visited[adj] = INSTACK;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
