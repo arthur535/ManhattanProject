fdd85637417bba274f89cafd6f75624feb25bdc6
Heisenberg <109768662+arthur535@users.noreply.github.com>
2024-01-26 01:08:51 +0400

    Create cycle_in_directed.cpp

---
+++ graph algorithms/cycle detection/cycle_in_directed.cpp
@@ -0,0 +1,28 @@
+
+bool has_cycle_directed(const vector<Node*>& vertices) {
+    unordered_map<Node*, bool> visited;
+    unordered_set<Node*> inStack;
+    for(int i = 0; i < vertices.size(); ++i){
+        Node* u = vertices[i];
+        if(!visited[u]) {
+            if(helper_dfs_for_directed_cycle(u, visited, inStack)) return true;
+        }
+    }
+    return false;
+}
+
+bool helper_dfs_for_directed_cycle(Node* u, unordered_map<Node*, bool>& visited, unordered_set<Node*>& inStack) {
+    visited[u] = true;
+    inStack.insert(u);
+    for(Node* neighbor : u->adjacents) {
+        cout << u->val << "  " << neighbor->val << endl;
+        if (inStack.count(neighbor)) {
+            return true;
+        }
+        if (!visited[neighbor] && helper_dfs_for_directed_cycle(neighbor, visited, inStack)) {
+           return true;
+        }
+    }
+    inStack.erase(u);
+    return false;
+}
